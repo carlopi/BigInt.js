a9bcf2b666bdb565f1efdcd731d0d02caeeb972c
Carlo Piovesan <carlo.piovesan@gmail.com>
2019-09-06 11:38:08 +0200

    Fix script + Compile both with -cheerp-pretty-code and not

--- recompile.sh
+++ recompile.sh
@@ -1,7 +1,7 @@
- rm -f libtommath/*.bc &&
 cd libtommath &&
 /opt/cheerp/bin/clang -target cheerp *.c -O3 -c &&
 cd .. &&
-/opt/cheerp/bin/clang++ -target cheerp -O3 C.cpp -c -Ilibtommath &&
-/opt/cheerp/bin/clang++ -target cheerp -O3 libtommath/*.bc C.bc -o BigInt.js
-
+/opt/cheerp/bin/clang++ -target cheerp -O3 BigInt.cpp -c -Ilibtommath &&
+/opt/cheerp/bin/clang++ -target cheerp -O3 libtommath/*.bc BigInt.bc -o BigInt.js -cheerp-pretty-code
+/opt/cheerp/bin/clang++ -target cheerp -O3 libtommath/*.bc BigInt.bc -o BigInt.min.js &&
+ rm -f libtommath/*.bc 
